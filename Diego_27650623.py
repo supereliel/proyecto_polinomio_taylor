c30a419b3d407b631a73cd4a847ff81e144f259d
DiegoTineo <56641947+DiegoTineo@users.noreply.github.com>
2019-11-17 19:31:47 -0400

    Update Diego_27650623.py

--- Diego_27650623.py
+++ Diego_27650623.py
@@ -32,10 +32,28 @@ def polinomio_taylor(f, x0, n):
     x0: punto centro del polinomio.
     n: grado del polinomio.
     """
-    
+    def polinomio(x):
+        p=f(x0) 
+        for i in range(n):  
+            if(i==0):
+                derivada_a = derivada(f)
+                p = p + (derivada_a(x0) * (x-x0))
+            else:
+                derivada_a = derivada(derivada_a)
+                p = p + ((derivada_a(x0) * (x-x0)**(i+1)) / math.factorial(i))
+        return p         
     return polinomio
 
 
 if __name__ == '__main__':
-    # Pruebe aquí el polinomio de Taylor.
+    # Pruebe aquí el polinomio de Taylor. Probado con el ejemplo dado en la clase
+    f = lambda x: math.sin(x)
+    x0=0
+    n = 4    
+    p = polinomio_taylor(f,x0,n)
+    x=0.3
+    print ("\n"+"POLINOMIO DE TAYLOR:")
+    print(" -Valor aproximado:",p(x))
+    print(" -Valor real:", f(x))
+    print(" -Error relativo", f(x)-p(x))
     pass
