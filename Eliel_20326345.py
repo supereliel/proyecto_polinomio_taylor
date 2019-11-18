0775f4258c84f19c0db8cede07a44f155967a068
Eliel <supereliel15@gmail.com>
2019-11-18 00:00:06 -0400

    implementación algoritmo

--- Eliel_20326345.py
+++ Eliel_20326345.py
@@ -8,6 +8,8 @@ Cada participante debe completar su módulo y luego solicitar el Pull-Request.
 import math
 
 
+
+
 def derivada(f, h = 0.01):
     """
     Retorna la función derivada de f dado un h.
@@ -32,10 +34,27 @@ def polinomio_taylor(f, x0, n):
     x0: punto centro del polinomio.
     n: grado del polinomio.
     """
-    
+
+    def polinomio(x):
+        j         = 0
+        resultado = 0
+        f_actual  = None
+        while(j < n):
+            if(j == 0):
+                resultado += f(x0)
+                f_actual   = f
+            else:
+                dj         = derivada(f_actual)
+                f_actual   = dj
+                resultado += ( (dj(x0) * math.pow((x-x0), j)) / math.factorial(j) )
+            j += 1
+        return resultado
     return polinomio
 
 
 if __name__ == '__main__':
     # Pruebe aquí el polinomio de Taylor.
-    pass
+    f1 = lambda x: math.sin(x)
+    #f1 = lambda x: 1/(1+x)
+    pt = polinomio_taylor(f1, 0, 4)
+    print("VALOR APROXIMADO UTILIZANDO POLINOMIO DE TAYLOR: ", pt(0.5))
